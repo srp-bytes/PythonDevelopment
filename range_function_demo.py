ed1e0a34ed7d049e9b34e2a57ae5f608214a67bd
Sujeeth,Pasham <spasham@microsoft.com>
2022-08-17 14:31:06 -0700

    using range(...

--- range_function_demo.py
+++ range_function_demo.py
@@ -2,3 +2,11 @@ numbers = [1,2,3,4,5]
 
 for number in range(0,len(numbers)):
     print(numbers[number])
+
+print("****************************")
+
+
+str_demo = "print a string by range index"
+
+for ch_idx in range(0, len(str_demo)):
+    print(str_demo[ch_idx])
