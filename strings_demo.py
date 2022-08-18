d84c4b07e8fe9f15bf0f2e83e5305d820c6a1a79
Sujeeth,Pasham <spasham@microsoft.com>
2022-08-18 15:29:23 -0700

    Usage of find() method and added new line after each print call

--- strings_demo.py
+++ strings_demo.py
@@ -1,15 +1,26 @@
 course = "learning PYTHON"
 
 print("Demo string: " + course)
+print()
 print("course.upper(): "+ course.upper())
+print()
 print("course.lower(): "+ course.lower())
+print()
 print("course.capitalize():" + course.capitalize())
+print()
 print("course.removesuffix(""python"")" + course.removesuffix("PYTHON"))
+print()
 print("course.removeprefix(""learning"")" + course.removeprefix("learning"))
+print()
 print("PYTHON" in course)
+print()
+print("print start index of Substring search: " + str(course.find("YTH")))
+print()
 
 temp = " string starts and ends with spaces"
+print()
 print(temp)
+print()
 print("Now trimming the spaces from the string using strip()" + temp.strip())
 
 print()
