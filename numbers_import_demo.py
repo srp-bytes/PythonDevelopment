151d0201cac7a42a82b6e70e2360dc3ab945d77b
Sujeeth,Pasham <spasham@microsoft.com>
2022-08-18 16:16:45 -0700

    Using import statements and math library

---
+++ numbers_import_demo.py
@@ -0,0 +1,20 @@
+import math
+
+PI = -3.14
+round_of_pi = round(PI)
+print("round of PI: " + str(round_of_pi))
+print()
+
+num1 = 3
+num2 = 2
+max_number = max(2, 3)
+print(f"max of {num1} , {num2}: " + str(max_number))
+print()
+
+min_number = min(2, 3)
+print(f"min of {num1} , {num2}: " + str(min_number))
+print()
+
+factorial_of_3 = str(math.factorial(3))
+
+print(f"Factorial({factorial_of_3})")
