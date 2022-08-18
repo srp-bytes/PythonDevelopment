00629d71fabf6c2174b59409aa03b8cc2282b7ac
Sujeeth,Pasham <spasham@microsoft.com>
2022-08-18 12:04:36 -0700

    Addition of for loop usage with strings

--- strings_demo.py
+++ strings_demo.py
@@ -7,3 +7,17 @@ print("course.capitalize():" + course.capitalize())
 print("course.removesuffix(""python"")" + course.removesuffix("PYTHON"))
 print("course.removeprefix(""learning"")" + course.removeprefix("learning"))
 print("PYTHON" in course)
+
+print()
+print("Indexed access of a string using range(start, end): ")
+print()
+
+for idx in range(0, len(course)):
+    print(course[idx])
+
+print()
+print("Printing each character in a string using in keyword")
+print()
+
+for char in course:
+    print(char)
