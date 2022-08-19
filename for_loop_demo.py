c2334718d11afa1f0ae7cf513a210fd0453998c2
Sujeeth,Pasham <spasham@microsoft.com>
2022-08-19 09:17:48 -0700

    break statement and for-else usage

--- for_loop_demo.py
+++ for_loop_demo.py
@@ -1,9 +1,27 @@
 str_demo = "print each character using a for loop"
-list_demo = [1,2,3,4,5]
+list_demo = [1, 2, 3, 4, 5]
 
 for chr in str_demo:
     print(chr)
 
-
 for num in list_demo:
     print(num)
+
+names = ["Kracken", "Seattle", "Austin", "John", "Mary"]
+
+print()
+
+for name in names:
+    if name.startswith("J"):
+        print("Found name which starts with J")
+        break
+
+print()
+
+for name in names:
+    if name.startswith("Z"):
+        print("Found name which starts with Z")
+        break
+
+else:
+    print("for-else executed as match was not found for the name")
